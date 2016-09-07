2d68e3ba9c04a9305c9f9a59d427cdc335f25465
ccwwonebyone <13852292859@163.com>
2016-09-07 13:39:38 +0800

    version1.0

--- database/migrations/2016_09_06_133702_creat_companyInfo_table.php
+++ database/migrations/2016_09_06_133702_creat_companyInfo_table.php
@@ -13,7 +13,19 @@ class CreatCompanyInfoTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('gl_company', function (Blueprint $table) {
+            $table->increments('id',8);
+            $table->char('name',255);
+            $table->char('tel',20);
+            $table->char('address', 255);
+            $table->char('qq',255)->nullable();
+            $table->char('weixin',255)->nullable('');
+            $table->char('sina',255)->nullable();
+            $table->char('belong',255)->nullable();
+            $table->date('create_time')->nullable();
+            $table->text('recommend')->nullable();
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -23,6 +35,6 @@ class CreatCompanyInfoTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('gl_company');
     }
 }
